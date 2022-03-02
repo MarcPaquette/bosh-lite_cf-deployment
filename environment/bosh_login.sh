117498e5693fef988a7038b935a0aa6fe65ea95a
Andrija Sagicc <andrija.sagic@gmail.com>
2022-03-02 18:16:15 +0100

    Update Vbox IP address following bosh-lite changes

--- environment/bosh_login.sh
+++ environment/bosh_login.sh
@@ -3,4 +3,4 @@ source ./environment/bosh_env.sh
 
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=`bosh int $DEPLOYMENT_DIRECTORY/creds.yml --path /admin_password`
-bosh alias-env $BOSH_ENV_ALIAS -e 192.168.50.6 --ca-cert <(bosh int $DEPLOYMENT_DIRECTORY/creds.yml --path /director_ssl/ca)
+bosh alias-env $BOSH_ENV_ALIAS -e 192.168.56.6 --ca-cert <(bosh int $DEPLOYMENT_DIRECTORY/creds.yml --path /director_ssl/ca)
